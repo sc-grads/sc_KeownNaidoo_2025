218298843cdf3676373ecf4d6407c414d02b603f
Keown-Sambe-Consulting <keown.naidoo@sambeconsulting.com>
2025-02-26 13:26:08 +0200

    Delete DatabaseAdministration/Testing_workflow/CRUD_Ops directory

--- DatabaseAdministration/Testing_workflow/CRUD_Ops/Insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (first_name, email, age) VALUES
-('Alice Johnson', 'alice@example.com', 25),
-('Bob Smith', 'bob@example.com', 30),
-('Charlie Brown', 'charlie@example.com', 28);
--- DatabaseAdministration/Testing_workflow/CRUD_Ops/Update.sql
+++
@@ -1,3 +0,0 @@
-UPDATE users 
-SET age = 26 
-WHERE id = 1;
