acd32c4222da7db63fd2c1e3e3e8ca53920bc4dc
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-03-10 14:27:13 +0200

    Modified Table- Added a Primary Key

--- DatabaseAdministration/Udemy_Database_Administration/Create_Table_(Used GUI).sql
+++ DatabaseAdministration/Udemy_Database_Administration/Create_Table_(Used GUI).sql
@@ -1,3 +1,5 @@
+/**************Before the Primary Key was added ********************/
+
 USE [OurFirstDatabase]
 GO
 
@@ -15,5 +17,29 @@ CREATE TABLE [dbo].[personalinfo](
 	[ID] [int] NOT NULL
 ) ON [PRIMARY]
 GO
+USE [OurFirstDatabase]
+GO
+
+/**************After the Primary Key was added ********************/
+
+/****** Object:  Table [dbo].[personalinfo]    Script Date: 2025/03/10 14:24:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[personalinfo](
+	[FirstName] [varchar](50) NULL,
+	[lastName] [varchar](50) NULL,
+	[dob] [datetime] NULL,
+	[ID] [int] NOT NULL,
+ CONSTRAINT [PK_personalinfo] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 
 
