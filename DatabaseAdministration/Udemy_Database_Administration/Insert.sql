3fc33cf846ec9d65f1c3bee80a66b1e61eda3261
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-03-10 14:30:59 +0200

    added Insert statement

---
+++ DatabaseAdministration/Udemy_Database_Administration/Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO [dbo].[personalinfo]
+           ([FirstName]
+           ,[lastName]
+           ,[dob]
+           ,[ID])
+     VALUES
+           ('Kevin'
+           ,'Stone'
+           ,'01/05/2005'
+           ,101)
+GO
+
+
