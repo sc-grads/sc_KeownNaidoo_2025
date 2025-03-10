4e6e2a9495bb3faa15bab2e84b8c70edaa6dcc1f
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-03-10 14:33:35 +0200

    Added more Insert statements

--- DatabaseAdministration/Udemy_Database_Administration/Insert.sql
+++ DatabaseAdministration/Udemy_Database_Administration/Insert.sql
@@ -9,5 +9,28 @@ INSERT INTO [dbo].[personalinfo]
            ,'01/05/2005'
            ,101)
 GO
+INSERT INTO [dbo].[personalinfo]
+           ([FirstName]
+           ,[lastName]
+           ,[dob]
+           ,[ID])
+     VALUES
+           ('Lisa'
+           ,'Stone'
+           ,'01/05/2005'
+           ,104)
+GO
+INSERT INTO [dbo].[personalinfo]
+           ([FirstName]
+           ,[lastName]
+           ,[dob]
+           ,[ID])
+     VALUES
+           ('Bom'
+           ,'Stone'
+           ,'01/05/2005'
+           ,181)
+GO
+
 
 
