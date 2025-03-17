47ce22e21287d9c7058a6fe4238d71f5bde0f66a
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-03-17 13:54:32 +0200

    Changed database to Adventure works instead of master

---
+++ DatabaseAdministration/SQLStatements/(UD-DA)DifferentialBackup.sql
@@ -0,0 +1,8 @@
+USE master
+
+--Differential Back up
+insert into SQLBackupRestoreTest (ID,loginname) values (6, 'test6')
+insert into SQLBackupRestoreTest (ID,loginname) values (7, 'test7')
+insert into SQLBackupRestoreTest (ID,loginname) values (8, 'test8')
+insert into SQLBackupRestoreTest (ID,loginname) values (9, 'test9')
+insert into SQLBackupRestoreTest (ID,loginname) values (10, 'test10')
--- DatabaseAdministration/SQLStatements/(UD-DA)FullBackup.sql
+++ DatabaseAdministration/SQLStatements/(UD-DA)FullBackup.sql
@@ -1,4 +1,5 @@
-CREATE TABLE SQLBackupRestoreTest (
+USE [AdventureWorks2022]
+CREATE TABLE SQLBackupRestoreTest1 (
 	ID INT NOT NULL PRIMARY KEY,
 	loginname VARCHAR(100) NOT NULL,
 	logindate DATETIME NOT NULL DEFAULT getdate()
@@ -6,13 +7,13 @@ CREATE TABLE SQLBackupRestoreTest (
 GO
 
 
-select *  from SQLBackupRestoreTest
+select *  from SQLBackupRestoreTest1
 -- 21 rows
-insert into SQLBackupRestoreTest (ID,loginname) values (1, 'test1')
-insert into SQLBackupRestoreTest (ID,loginname) values (2, 'test2')
-insert into SQLBackupRestoreTest (ID,loginname) values (3, 'test3')
-insert into SQLBackupRestoreTest (ID,loginname) values (4, 'test4')
-insert into SQLBackupRestoreTest (ID,loginname) values (5, 'test5')
+insert into SQLBackupRestoreTest1 (ID,loginname) values (1, 'test1')
+insert into SQLBackupRestoreTest1 (ID,loginname) values (2, 'test2')
+insert into SQLBackupRestoreTest1 (ID,loginname) values (3, 'test3')
+insert into SQLBackupRestoreTest1 (ID,loginname) values (4, 'test4')
+insert into SQLBackupRestoreTest1 (ID,loginname) values (5, 'test5')
 -- FULL Back up 5 rows
 
 BACKUP DATABASE [master] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\master.bak'
