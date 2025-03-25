8e4507c52a18a1f6207978670444fa16224f70f3
Keown-Sambe-Consulting <keown.naidoo@sambeconsulting.com>
2025-03-25 20:47:07 +0200

    Update 2_CreateTable.sql

--- DatabaseAdministration/DatabaseAutomation/2_CreateTable.sql
+++ DatabaseAdministration/DatabaseAutomation/2_CreateTable.sql
@@ -2,7 +2,7 @@ USE [AutoTest]
 GO
 
 CREATE TABLE [user] (
-    Name NVARCHAR(50) NOT NULL,
+    FirstName NVARCHAR(50) NOT NULL,
     Surname NVARCHAR(50) NOT NULL,
     Email NVARCHAR(100) NOT NULL
 );
