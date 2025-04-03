8f7a85ee10821a6602645a44ff974a6f029928da
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-04-03 15:10:58 +0200

    updated SQL script

--- DatabaseAdministration/DatabaseAutomation/3_StoredProcedure.sql
+++ DatabaseAdministration/DatabaseAutomation/3_StoredProcedure.sql
@@ -1,12 +1,20 @@
-USE [AutoTest_KeownNaidoo]
+USE [AutoTest_KeownNaidoo];
 GO
-CREATE PROCEDURE sp_InsertUser
-    @FirstName NVARCHAR(50),  -- Changed to match column
-    @Surname NVARCHAR(50),
-    @Email NVARCHAR(100)
-AS
-BEGIN
-    INSERT INTO [user] (FirstName, Surname, Email)
-    VALUES (@FirstName, @Surname, @Email);  -- Parameters match columns
-END
+
+BEGIN TRY
+    CREATE PROCEDURE sp_InsertUser
+        @FirstName NVARCHAR(50),
+        @Surname NVARCHAR(50),
+        @Email NVARCHAR(100)
+    AS
+    BEGIN
+        INSERT INTO [user] (FirstName, Surname, Email)
+        VALUES (@FirstName, @Surname, @Email);
+    END;
+    PRINT 'Stored procedure sp_InsertUser created successfully.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error occurred while creating the stored procedure.';
+    PRINT ERROR_MESSAGE(); -- Displays the actual error message
+END CATCH;
 GO
