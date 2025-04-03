f5c2476668fb36fb3c23cb3da9a0adc5f94fffb5
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-04-03 15:08:24 +0200

    updated SQL script

--- DatabaseAdministration/DatabaseAutomation/1_CreateDatabase.sql
+++ DatabaseAdministration/DatabaseAutomation/1_CreateDatabase.sql
@@ -1,7 +1,12 @@
---This is for creating database
-USE master
-GO
- 
-CREATE DATABASE AutoTest_KeownNaidoo
+USE master;
 GO
 
+BEGIN TRY
+    CREATE DATABASE AutoTest_KeownNaidoo;
+    PRINT 'Database created successfully.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error occurred while creating the database.';
+    PRINT ERROR_MESSAGE(); -- Displays the actual error message
+END CATCH;
+GO
--- DatabaseAdministration/DatabaseAutomation/2_CreateTable.sql
+++ DatabaseAdministration/DatabaseAutomation/2_CreateTable.sql
@@ -1,9 +1,16 @@
-USE [AutoTest_KeownNaidoo]
+USE [AutoTest_KeownNaidoo];
 GO
 
-CREATE TABLE [user] (
-    FirstName NVARCHAR(50) NOT NULL,
-    Surname NVARCHAR(50) NOT NULL,
-    Email NVARCHAR(100) NOT NULL
-);
+BEGIN TRY
+    CREATE TABLE [user] (
+        FirstName NVARCHAR(50) NOT NULL,
+        Surname NVARCHAR(50) NOT NULL,
+        Email NVARCHAR(100) NOT NULL
+    );
+    PRINT 'Table [user] created successfully.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error occurred while creating the table.';
+    PRINT ERROR_MESSAGE(); -- Displays the actual error message
+END CATCH;
 GO
