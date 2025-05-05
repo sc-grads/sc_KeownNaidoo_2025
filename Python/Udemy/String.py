3cb4e866b79758bae84fb44d80c33d2c218b983e
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-05-05 09:51:10 +0200

    Added Python Files

---
+++ Python/Udemy/String.py
@@ -0,0 +1,20 @@
+name = 'Mario \'s'
+fruit = "banana's"
+
+print(name + fruit)
+
+poem = """
+this is a poem
+a very funny poem
+laugh , hahahhaa
+"""
+
+print(poem)
+
+#type converion
+
+text = '100'
+num =50
+
+print(int(text)+num)
+print(text+str(num))
