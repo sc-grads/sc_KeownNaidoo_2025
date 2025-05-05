ece30c1e5be9f56a20f9bfdfff325fc2481cbdea
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-05-05 09:45:27 +0200

    Added Python Files

---
+++ Python/Udemy/integer.py
@@ -0,0 +1,39 @@
+age :int = 30
+money : int = 100
+a: int = 5
+b: int =10
+
+print(a+b)
+print(a-b)
+
+print(type(a/b))
+
+c:float=1.25
+print(a+c)
+
+#OPERATORS
+print(a//b)
+print(a**b) #power
+print(a%b)
+
+a+=5
+
+
+#COMPARISON
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#LOGICAL Operators
+
+print(a>c and b>a)
+print(a>c or b>a)
+print(not(a>c))
+
+
+
+
