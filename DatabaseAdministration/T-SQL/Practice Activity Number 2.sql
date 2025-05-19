40c54b9f1a45fbb5eb150f81f1ea33fdd6182291
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-05-19 10:01:20 +0200

    Modified Sql script

---
+++ DatabaseAdministration/T-SQL/Practice Activity Number 2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tblPrimeNumber
+(intField int)
+go
+
+insert into tblPrimeNumber values (2),(3),(5),(7)
+go
+
+select * from tblPrimeNumber
+go
+
+delete from tblPrimeNumber
+go
+
+drop table tblPrimeNumber
