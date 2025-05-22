eee4407d148175d69bd1584e93bf6d5212dc6bd5
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-05-22 15:29:58 +0200

    Modified Sql script

---
+++ DatabaseAdministration/T-SQL/XML_in_Select.sql
@@ -0,0 +1,23 @@
+select E.EmployeeNumber, E.EmployeeFirstName, E.EmployeeLastName
+	   , E.DateOfBirth, T.Amount, T.DateOfTransaction
+from [dbo].[tblEmployee] as E
+left join [dbo].[tblTransaction] as T
+on E.EmployeeNumber = T.EmployeeNumber
+where E.EmployeeNumber between 200 and 202
+for xml raw
+
+select E.EmployeeNumber, E.EmployeeFirstName, E.EmployeeLastName
+	   , E.DateOfBirth, T.Amount, T.DateOfTransaction
+from [dbo].[tblEmployee] as E
+left join [dbo].[tblTransaction] as T
+on E.EmployeeNumber = T.EmployeeNumber
+where E.EmployeeNumber between 200 and 202
+for xml raw('MyRow')
+
+select E.EmployeeNumber, E.EmployeeFirstName, E.EmployeeLastName
+	   , E.DateOfBirth, T.Amount, T.DateOfTransaction
+from [dbo].[tblEmployee] as E
+left join [dbo].[tblTransaction] as T
+on E.EmployeeNumber = T.EmployeeNumber
+where E.EmployeeNumber between 200 and 202
+for xml raw('MyRow'), elements
