1a0f328d86b06f621a5db58a88d2666ec6230622
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-06-05 15:38:46 +0200

    Added SSIS packages

--- Hands-on Projects/Timesheet/SQL/Select.sql
+++ Hands-on Projects/Timesheet/SQL/Select.sql
@@ -4,3 +4,23 @@ SELECT*
   truncate table Timesheet
 
   drop table Timesheet
+
+
+  SELECT*
+  FROM [dbo].[Client]
+
+    truncate table Client
+
+
+    SELECT*
+  FROM [dbo].[Employee]
+
+    truncate table Employee
+
+	
+    SELECT*
+  FROM [dbo].[Leave]
+
+    truncate table [dbo].[Leave]
+
+	drop table Leave
