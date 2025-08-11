91ae554868168a98fecd907c9341fdf30eb861ea
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-11 15:47:58 +0200

    added file

---
+++ C#/Random/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// creating an instance of the Random Class
+Random random = new Random();
+// this will give us a random  number between 1 and 10
+int randomNumber = random.Next(1, 11);
+
+Console.WriteLine("Guess the number");
+
+string inputString = Console.ReadLine();
+int num1 = 0;
+
+bool isNumber = int.TryParse(inputString, out num1);
+
+if (isNumber)
+{
+    if (num1 == randomNumber)
+    {
+        Console.WriteLine("You guessed right!");
+    }
+    else
+    {
+        Console.WriteLine("You guessed wrong, try again!");
+    }
+}
+else
+{
+    Console.WriteLine("Haha you troll. You should've entered a number");
+}
+Console.ReadKey();
