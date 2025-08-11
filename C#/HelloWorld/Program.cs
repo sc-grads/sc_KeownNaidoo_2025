9aefb4aa7693023c5e59869313e02e06d2c2652f
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-11 08:45:14 +0200

    added file

--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -1,7 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
 string fName = "John";
+
+Console.WriteLine(fName);
+
+fName = "bob";
 
 Console.WriteLine(fName);
 
