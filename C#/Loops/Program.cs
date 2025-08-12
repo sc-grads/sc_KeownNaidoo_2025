348c5c7844b8acc9e3b981e184ea85501076eed9
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-12 09:13:50 +0200

    added file

---
+++ C#/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// in strings \ is an "Escape Character"
+// \n stands for "new line"
+// \r - carriage return
+
+//string myString = "Hi \r\nHi";
+
+//for (int counter = 10; counter >= 0; counter--)
+//{
+//    Console.WriteLine("Counter is " + counter);
+//    Thread.Sleep(1000);
+//}
+
+Console.WriteLine("Enter go or stay");
+
+string userChoice = Console.ReadLine();
+
+while (userChoice == "go")
+{
+    Console.WriteLine("Go for a mile");
+    Console.WriteLine("Wanna keep going? Enter go.");
+    userChoice = Console.ReadLine();
+}
+Console.WriteLine("Finally you are staying!");
+
+
+
+Console.ReadKey();
