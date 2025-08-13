809846c73d9ea2781095ee79ee2365cb1ed7ef43
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-13 15:08:35 +0200

    added file

---
+++ C#/Classes/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+       
+
+        public Customer()
+        {
+            Name = "New Customer";
+            Address = "Unknown";
+            ContactNumber = "None";
+        }
+
+
+
+        public Customer(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ C#/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassesApp;
+
+namespace Classes
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Customer earl = new Customer("Earl");
+
+            Customer frankTheTank = new Customer("FrankTheTank", "Mainstreet 1", "5551234567");
+
+            Console.WriteLine("Name of Customer is " + earl.Name);
+
+            Customer myCustomer = new Customer();
+
+            Console.WriteLine("Please enter the customers Name");
+
+            myCustomer.Name = Console.ReadLine();
+
+            Console.WriteLine("Details about customer: " + myCustomer.Name);
+
+
+            Console.ReadKey();
+        }
+    }
+}
