0897bdafa0d944a10a48bca322cae23eb128e832
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-13 09:10:54 +0200

    added file

---
+++ C#/Oldstyle/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Oldstyle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myResult;
+
+            Console.WriteLine("Enter a number, I'll add 10 to it!");
+            int num1 = int.Parse(Console.ReadLine());
+
+            myResult = AddTwoValues(num1, 10);
+            Console.WriteLine("The result is " + myResult);
+
+            Console.ReadKey();
+        }
+
+        static int SubtractTwoValues(int value1, int value2)
+        {
+            int result = (value1 - value2);
+            return result;
+        }
+        static int AddTwoValues(int value1, int value2)
+        {
+            int result = (value1 - value2);
+            return result;
+        }
+    }
+}
