b5b0046d36826307cbc26c4a3d164526da5c5bbe
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-15 09:43:16 +0200

    added file

---
+++ C#/Lists/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> list = ["a", "b", "c", "d", "e"];
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //list.Remove("a");
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            List<int> list2 =[1,5,7,9,3,6,2,4] ;
+
+            list2.Sort();
+
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<int> x = list2.FindAll(x => x >= 5);
+
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+
+            bool boo = list2.Any(x => x >= 5);
+
+            Console.ReadKey();
+        }
+    }
+}
