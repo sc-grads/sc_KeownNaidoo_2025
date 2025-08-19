f0c21c1b387af3df9596d42a53b62428d3623186
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-19 16:00:09 +0200

    added file

---
+++ C#/Events2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Events2
+{
+    public delegate void TemperatureChangeHandler(string msg);
+
+    public class TempMonitor
+    {
+        public event TemperatureChangeHandler OnTemperatureChange;
+        private int temp;
+
+        public int Temp
+        {
+            get { return temp; }
+            set
+            {
+
+                value = temp;
+                RaisedTemp("Changed");
+
+
+
+            }
+        }
+
+        protected virtual void RaisedTemp(string msg)
+        {
+
+            OnTemperatureChange?.Invoke(msg);
+
+        }
+
+
+    }
+
+    public class Sub
+    {
+        public void OnTempChange(string msg)
+        {
+            Console.WriteLine("Temp change :" + msg);
+        }
+
+
+
+        internal class Program
+        {
+
+            static void Main(string[] args)
+            {
+                TempMonitor tempMonitor = new TempMonitor();
+                Sub sub = new Sub();
+                tempMonitor.OnTemperatureChange += sub.OnTempChange;
+
+                tempMonitor.Temp = 100;
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
