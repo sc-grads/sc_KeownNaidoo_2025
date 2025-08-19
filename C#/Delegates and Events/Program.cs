ddadb7f34358182ce2c51c1ef4bffe0a7e4d62b2
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-19 10:32:49 +0200

    added file

---
+++ C#/Delegates and Events/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates_and_Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
