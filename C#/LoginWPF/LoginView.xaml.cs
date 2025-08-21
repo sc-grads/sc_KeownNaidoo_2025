34eff85a7d346b777879cee96e33bb47a94696eb
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-21 15:56:00 +0200

    added file

---
+++ C#/LoginWPF/LoginView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginWPF
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string pword = txtPass.Password;
+            //string? password = Environment.GetEnvironmentVariable("Invoice");
+            string? password = "123456789";
+
+            if (!string.IsNullOrEmpty(pword) && string.IsNullOrEmpty(password)) {
+
+                if (pword == password)
+                {
+                    MessageBox.Show("Password is correct");
+                     
+                }
+                else { 
+                
+                    MessageBox.Show("Password is incorrect");
+
+                }
+
+
+            }
+        }
+
+        private void txtPass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            btnLogin.IsEnabled = !string.IsNullOrEmpty(txtPass.Password);
+        }
+    }
+}
--- C#/LoginWPF/Success.xaml.cs
+++ C#/LoginWPF/Success.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,14 +16,13 @@ using System.Windows.Shapes;
 namespace LoginWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Success.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Success : UserControl
     {
-        public MainWindow()
+        public Success()
         {
             InitializeComponent();
-            MainContent.Content = new LoginView();
         }
     }
 }
