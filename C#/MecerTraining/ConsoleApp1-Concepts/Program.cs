46f9fef81b8bdc179845eceb08079d4c3f8f7f93
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-08-25 11:30:20 +0200

    Refactor Program.cs and update .NET dependencies
    Significantly modified `Program.cs` by removing unused variable declarations and introducing new arithmetic operations with a constant integer. Several lines were commented out for potential refactoring.
    
    Added `ConsoleApp1-Concepts.deps.json` and `ConsoleApp1-Concepts.runtimeconfig.json` to define the runtime target as .NET Core 9.0, ensuring compatibility with the updated framework.

--- C#/MecerTraining/ConsoleApp1-Concepts/Program.cs
+++ C#/MecerTraining/ConsoleApp1-Concepts/Program.cs
@@ -5,18 +5,35 @@
         static void Main(string[] args)
         {
             int num = 15;
-            long x = 289;
-            short y = 23;
+            //Console.WriteLine(num);
 
-            double Salary = 1500.99d;
-            decimal price = 15.99m;
-            float point = 18.9989f;
+            const int number = 20; // cant change
 
-            bool isRight = false;
+            int result = number + num;
+
+            result = result + 10;
+            result += 10; // same as the above line -= ; *= ; /= ; %=
+
+            double mod = 5.00 % 2.00;
+
+            bool isGreater = 5>0;
+
+            Console.WriteLine(mod);
 
 
-            string name= "john";
-            char letter = 'A';
+            //long x = 289;
+            //short y = 23;
+
+            //double Salary = 1500.99d;
+            //decimal price = 15.99m;
+            //float point = 18.9989f;
+
+            //bool isRight = false;
+
+
+            //string name= "john";
+            //char letter = 'A';
+
 
 
 
