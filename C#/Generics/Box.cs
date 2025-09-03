512f98063ba7ac2cfe8bb7c6b5f13f49df2362dc
Keown_Naidoo <keown.naidoo@sambeconsulting.com>
2025-09-03 09:44:47 +0200

    Created first gerenic

---
+++ C#/Generics/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Box<T>
+    {
+        public T Content { get; set; }
+
+        public string Log()
+        {
+            return $"Box contains: {Content}";
+        }
+    }
+}
---
+++ C#/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> intBox = new Box<int>();
+            intBox.Content = 123;
+
+            Box<string> strBox = new Box<string>();
+            strBox.Content = "Hello, Generics!";
+
+            Console.WriteLine(strBox.Log());
+
+            Console.WriteLine( intBox.Log());
+
+            Console.ReadLine();
+        }
+    }
+}
